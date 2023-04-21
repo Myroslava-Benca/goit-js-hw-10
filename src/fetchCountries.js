162ee624a94409c2b50b24984be1364b3816bf0e
Myroslava-Benca <mashaivanova685@gmail.com>
2023-04-21 20:02:14 +0300

    n

---
+++ src/fetchCountries.js
@@ -0,0 +1,11 @@
+
+const BASE_URL = `https://restcountries.com/v3.1/name/`;
+const fields = `fields=name, capital , population , flags , languages`;
+
+export function fetchCountries(name) {
+    return fetch('${BASE_URL}${name}?${fields}')
+        .then(response => response.json()
+        .then(response => response[0])
+        .then(({ name }) => console.log(name)));
+
+    } 
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import './css/styles.css';
+import  fetchCountries from './fetchCountries.js';
+
+// const DEBOUNCE_DELAY = 300;
+
+let name = '';
+
+const refs = {
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+    input: document.querySelector('.input')
+}
+
+refs.input.addEventListener('input', onSearch)
+
+function onSearch(evt){
+  evt.preventDefault()
+    name = evt.target.value.trim();
+
+    fetchCountries()
+        .then(renderCountry)
+        .catch(error)
+}
+
+function renderCountry({name, capital , population , flags , languages}) {
+    return ` <ul>
+    <li> 
+    <img src =${flags.svg} alt='flags of ${name.official}' width=60 height=40/>
+     </li>
+     </ul>
+     <ul>
+     <li>Capital: ${capital}</li>
+     <li>Population: ${population}</li>
+     <li>Languages: ${languages}</li>
+     </ul>
+     `
+}
+
